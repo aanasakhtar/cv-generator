cab7720b11723350693cf20f8653c2d3a0783add
aanasakhtar <anasakhtar851@gmail.com>
2024-07-10 16:38:12 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,70 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <section>
+          <h3>Basic Information</h3>
+          <div>
+            <label htmlFor="name" className='data-label'>Name</label>
+            <input type="text" id='name' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="occupation" className='data-label'>Occupation</label>
+            <input type="text" id='occupation' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="email" className='data-label'>Email</label>
+            <input type="text" id='email' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="number" className='data-label'>Number</label>
+            <input type="number" id='number' className='input-data'/>
+          </div>
+        </section>
+        <section>
+          <h3>Education</h3>
+          <div>
+            <label htmlFor="school" className='data-label'>School</label>
+            <input type="text" id='school' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="major" className='data-label'>Major</label>
+            <input type="text" id='major' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="date" className='data-label'>Date</label>
+            <input type="date" id='date' className='input-data'/>
+          </div>
+          <div className='buttons'>
+            <button className='add-more-edu btn'>Add more education</button>
+            <button className='remove-edu btn'>Remove education</button>
+          </div>
+        </section>
+        <section>
+          <h3>Experience</h3>
+          <div>
+            <label htmlFor="company" className='data-label'>Company</label>
+            <input type="text" id='company' className='input-data'/>
+          </div>
+          <div>
+            <label htmlFor="responsibilities" className='data-label'>Responsibilities</label>
+            <input type="text" id='responsibilities' className='input-data'/>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="starting-date" className='data-label'>Starting date</label>
+              <input type="date" id='starting-date' className='input-data'/>
+              <label htmlFor="ending-date" className='data-label'>Ending date</label>
+              <input type="date" id='ending-date' className='input-data'/>
+            </div>
+          </div>
+          <div className='buttons'>
+            <button className='add-more-exp btn'>Add more experience</button>
+            <button className='remove-exp btn'>Remove experience</button>
+          </div>
+          
+        </section>
+      </form>
     </>
   )
 }
